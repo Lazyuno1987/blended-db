4972468df9e2363f67c555aa8b4e8066ba4ae448
DoctorDenis <sychovdenis@icloud.com>
2022-10-30 13:05:40 +0200

    Changed expiration duration of token

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,8 +1,8 @@
-const bcrypt = require('bcryptjs');
-const { Unauthorized } = require('http-errors');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const { Unauthorized } = require("http-errors");
+const jwt = require("jsonwebtoken");
 
-const { User } = require('../../models');
+const { User } = require("../../models");
 
 const { SECRET_KEY } = process.env;
 
@@ -15,18 +15,18 @@ const login = async (req, res) => {
   const passComparePassword = bcrypt.compareSync(password, user.password);
 
   if (!user || !passComparePassword) {
-    throw new Unauthorized('Email or password is wrong');
+    throw new Unauthorized("Email or password is wrong");
   }
 
   const payload = {
     id: user._id,
   };
 
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1d" });
   await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
-    status: 'success',
+    status: "success",
     code: 200,
     data: {
       token,
